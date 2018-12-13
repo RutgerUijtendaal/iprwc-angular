6cfa88a477123e3697a905b0d9f358c6ab971edb
RutgerUijtendaal <Rutger.uijtendaal@gmail.com>
2018-12-13 13:23:20 +0100

    Started architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CoreModule} from './core/core.module';
+import {SharedModule} from './shared/shared.module';
+import {ShopModule} from './shop/shop.module';
+import {AdminModule} from './admin/admin.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    ShopModule,
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
